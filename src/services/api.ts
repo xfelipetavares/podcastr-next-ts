e4acd434ac0208e9ccf6b55f76000ca82b2db554
Felipe Tavares <xfelipetavares@gmail.com>
2023-07-14 22:32:53 -0300

    server added

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const api = axios.create({baseURL: 'http://localhost:3333/'});
+const api = axios.create({baseURL: 'https://my-json-server.typicode.com/xfelipetavares/podcastr-next-ts/'});
 
 export default api;
